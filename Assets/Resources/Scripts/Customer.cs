46d865fc7f53f66223dcf1a1a6785c697a200924
unknown <gustncjswp95@gmail.com>
2019-08-08 14:19:42 +0900

    Added Customer Data

---
+++ Assets/Resources/Scripts/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//
+using SimpleJSON;
+
+public class Customer : MonoBehaviour
+{
+    public SpriteRenderer body;
+    public SpriteRenderer face;
+    public SpriteRenderer emote;
+    public SpriteRenderer hair;
+
+    private void Start()
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>("Data/customers");
+        string jsonString = textAsset.text;
+        JSONNode node = JSON.Parse(jsonString);
+
+        Init(new CustomerInfo(node[0]));
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void Init(CustomerInfo info)
+    {
+        body.sprite = Resources.Load<Sprite>("Images/Customer/Body/" + info.body);
+        face.sprite = Resources.Load<Sprite>("Images/Customer/Face/" + info.face);
+        emote.sprite = Resources.Load<Sprite>("Images/Customer/Emote/" + info.emote);
+        hair.sprite = Resources.Load<Sprite>("images/Customer/Hair/" + info.hair);
+    }
+}
---
+++ Assets/Resources/Scripts/CustomerInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class CustomerInfo
+{
+    public string name;
+    public int mentalPoint;
+    public int physicalPoint;
+    public string want;
+    public string need;
+    public string sick;
+    public string body;
+    public string face;
+    public string emote;
+    public string hair;
+    public string script;
+    public string advice;
+    public int[,] point = new int[3,2];
+
+    public CustomerInfo(JSONNode node)
+    {
+        int typeLength = node["status"].Count;
+        int type = Random.Range(0, typeLength);
+
+        name = node["name"].Value;
+        mentalPoint = node["mental_point"].AsInt;
+        physicalPoint = node["physical_point"].AsInt;
+        want = node["status"][type]["want"].Value;
+        need = node["status"][type]["need"].Value;
+        sick = node["status"][type]["sick"].Value;
+        body = node["status"][type]["body"].Value;
+        face = node["status"][type]["face"].Value;
+        emote = node["status"][type]["emote"].Value;
+        hair = node["status"][type]["hair"].Value;
+        script = node["status"][type]["script"].Value;
+        advice = node["status"][type]["advice"].Value;
+
+        for (int i = 0; i < 3; i++)
+        {
+            point[i, 0] = node["status"][type]["point"]["mental_point"].AsInt;
+            point[i, 1] = node["status"][type]["point"]["physical_point"].AsInt;
+        }
+    }
+}
+
+//facial expression : 네이버 블로그
