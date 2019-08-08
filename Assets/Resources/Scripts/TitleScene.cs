b95ce7b545729d1e74fb4e9ae390d9a1b3ff94cd
unknown <gustncjswp95@gmail.com>
2019-08-09 04:41:07 +0900

    Added Title/Credit/HowToPlay Scene

---
+++ Assets/Resources/Scripts/TitleScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickHowToPlay()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void OnClickCredit()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
