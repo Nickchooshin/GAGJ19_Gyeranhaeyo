918e6fae3412002a7f26f702cef38367a5a80347
unknown <gustncjswp95@gmail.com>
2019-08-09 10:35:24 +0900

    Fixed Game Timer Sub Per Stage

--- Assets/Resources/Scripts/TimeManager.cs
+++ Assets/Resources/Scripts/TimeManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class TimeManager : MonoBehaviour
 {
     public float dayTime = 50.0f;
+    public float subTimePerStage = 5.0f;
+    public float minimumTime = 30.0f;
     public Text timer;
 
     private float m_deltaTime = 0.0f;
@@ -28,30 +30,38 @@ public class TimeManager : MonoBehaviour
         StartCoroutine(Timer());
     }
 
-    public bool IsTimeOver()
-    {
-        return m_deltaTime >= dayTime;
-    }
-
     private IEnumerator Timer()
     {
         m_deltaTime = 0.0f;
 
-        while (m_deltaTime < dayTime) 
+        while (m_deltaTime < GetTimerTime()) 
         {
             m_deltaTime += Time.deltaTime;
-            timer.text = string.Format("{0:00.00}", (dayTime - m_deltaTime));
+            timer.text = string.Format("{0:00.00}", (GetTimerTime() - m_deltaTime));
 
             yield return null;
         }
 
-        timer.text = string.Format("{0:00.00}", dayTime);
         CustomerManager.Instance.StopAll();
         GameManager.Instance.EndOfDay();
+        timer.text = string.Format("{0:00.00}", GetTimerTime());
     }
 
     public int GetTimerScore()
     {
         return (int)(dayTime - m_deltaTime);
+    }
+
+    private float GetTimerTime()
+    {
+        float time = dayTime;
+        float sub = subTimePerStage * GameManager.Instance.nowDay;
+
+        time -= sub;
+
+        if (time < minimumTime)
+            time = minimumTime;
+
+        return time;
     }
 }
